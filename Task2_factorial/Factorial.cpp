b2ba1e19a40a264cd1bd2a790571ad50be243f5b
/rdndev <rdninc@mail.ru>
2021-03-30 08:36:00 +0300

    Create Factorial.cpp

---
+++ Task2_factorial/Factorial.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+unsigned long long factorial(int k);
+
+int main()
+{
+    std::cout << "Factorial (5) = " << factorial(5);
+    system("pause > nul");
+}
+
+// Факторил числа.
+// k - число.
+unsigned long long factorial(int k) {
+    if (k < 0)
+        throw 777;
+    if (k == 0)
+        return 1;
+    return k * factorial(k - 1);
+}
