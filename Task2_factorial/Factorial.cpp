cc655848bb7bb796d550bb4086b4d01b067602b4
/rdndev <rdninc@mail.ru>
2021-03-30 08:48:04 +0300

    Update Factorial.cpp
    upd (new version)

--- Task2_factorial/Factorial.cpp
+++ Task2_factorial/Factorial.cpp
@@ -1,18 +1,34 @@
 ﻿#include <iostream>
-unsigned long long factorial(int k);
+unsigned long long factorial(int);
+int fact(int, int = 1);
 
 int main()
 {
-    std::cout << "Factorial (5) = " << factorial(5);
+    int n;
+    std::cout << "Enter n = ";
+    std::cin >> n;
+    std::cout << "Factorial (" << n << ") = " << factorial(n) << std::endl;
     system("pause > nul");
 }
 
-// Факторил числа.
-// k - число.
-unsigned long long factorial(int k) {
-    if (k < 0)
-        throw 777;
-    if (k == 0)
+// Factorial of a number.
+// k - number.
+unsigned long long factorial(int n) {
+    if (n < 0)
+        throw -1; // Error number.
+    if (n == 0)
         return 1;
-    return k * factorial(k - 1);
+    return n * factorial(n - 1);
+}
+
+// Factorial of a number.
+// k - number.
+int fact(int n, int result) {
+    // Tail recursion.
+    if (n < 0)
+        throw - 1; // Error number.
+    if (n == 0 || n == 1) 
+        return result;
+    else
+        return fact(n - 1, n * result);
 }
